d9d73b61d9d4481d1d16386e69b2973b2d505154
Pavel Palancica <pavel.palancica@gmail.com>
2020-05-26 22:27:11 +0300

    first commit

---
+++ Swift-Data-Structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Data-Structures
+//
+//  Created by Pavel Palancica on 5/26/20.
+//  Copyright © 2020 I Dev TV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
